97b43811c0d640fbcbf7fd63c61da44ef2f80b37
Karan <packirisamykaran@gmail.com>
2023-11-06 21:38:28 +0800

    total users

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import telebot
 
 # Get your bot token from BotFather: https://telegram.me/BotFather
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
+BOT_TOKEN = ''
 
 # Create a new TeleBot object
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -9,35 +9,26 @@ bot = telebot.TeleBot(BOT_TOKEN)
 # Define a function to get the active users of a chat group
 
 
-def get_active_users(chat_id):
-    """Gets the active users of a chat group.
-
-    Args:
-        chat_id: The ID of the chat group.
-
-    Returns:
-        A list of the active users of the chat group.
-    """
-
-    active_users = []
-    for member in bot.get_chat_members(chat_id):
-        if member.status == 'online':
-            active_users.append(member.user)
-    return active_users
-
-# Define a command handler for the bot
-
-
-@bot.message_handler(commands=['active_users'])
-def active_users_command(message):
+@bot.message_handler(commands=['total_users'])
+def group_users_command(message):
     """Gets the active users of the chat group."""
 
     chat_id = message.chat.id
-    active_users = get_active_users(chat_id)
+    user_id = message.from_user.id
 
-    response = 'The active users of this chat group are:\n'
-    for user in active_users:
-        response += f'- {user.first_name}\n'
+    response = f'total users in the chat is {bot.get_chat_member_count(chat_id)}:\n'
+
+    bot.send_message(chat_id, response)
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    """Gets the active users of the chat group."""
+
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    response = 'what would you like to know\n'
 
     bot.send_message(chat_id, response)
 
